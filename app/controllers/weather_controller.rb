f318a44331bcd1a4c3b9fdc2e9f2ebe06febb88a
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-21 11:36:13 -0500

    added instance variable @forecast and set it to our local zip. also added @forecast to index.html.erb to show the weather on the page

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,6 +2,7 @@ class WeatherController < ApplicationController
 
   def index
   	w_api = Wunderground.new("b559451ca25b9914")
+  	@forecast = w_api.forecast_and_conditions_for("19143")
   end
 
 end
