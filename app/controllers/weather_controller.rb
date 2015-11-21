c4db56bb7be72efda89bef6c3fcf060b93963f50
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-21 12:34:58 -0500

    created remote_ip & random_location methods. remote_ip (on production) will get user's ip address from request. random_location is just using a few random locations as samples for weather until we have users with their own locations.

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,24 @@ class WeatherController < ApplicationController
 
   def index
   	w_api = Wunderground.new(Rails.application.secrets.wapi_key)
-  	@forecast = w_api.forecast_and_conditions_for("19143")
+  	@forecast = w_api.forecast_and_conditions_for(random_location)
   end
+
+	private 
+	#Get the IP Address of the User (or, locally, hard-code ours in)
+	def remote_ip
+	  if request.remote_ip == '127.0.0.1' || request.remote_ip == '::1'
+	  	# My Local Machine
+	  	'76.124.141.194'
+	  else
+	  	requests.remote_ip
+	  end
+	end
+
+	# Get a random location from a list of locations
+	def random_location
+	  options = ["WA/Spokane", "CA/San Francisco", "FL/Miami", "CO/Denver"]
+	  options.sample
+	end
 
 end
