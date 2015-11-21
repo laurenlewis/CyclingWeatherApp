9a4cb2d426c4184591e198215f46eabdffc64020
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-21 12:14:20 -0500

    securing api key

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,7 +1,7 @@
 class WeatherController < ApplicationController
 
   def index
-  	w_api = Wunderground.new("b559451ca25b9914")
+  	w_api = Wunderground.new(Rails.application.secrets.wapi_key)
   	@forecast = w_api.forecast_and_conditions_for("19143")
   end
 
