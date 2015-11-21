2deaac6ed856a3684a4b6183fd705fc7f7be206d
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-21 13:42:32 -0500

    created way to debug @forecast response data & decide what should go in our own hash, & then display on the page. also created @recommendation as the beginning of what we could recommend to user

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,12 +3,30 @@ class WeatherController < ApplicationController
   	w_api = Wunderground.new(Rails.application.secrets.wapi_key)
 
   	# If we just want to use the user's current location
-  	location = { :geo_ip => remote_ip }
+    # location = { :geo_ip => remote_ip }
 
   	# For testing, use a random location
-  	# location = random_location
+  	location = random_location
 
   	@forecast = w_api.forecast_and_conditions_for(location)
+  	@todays_forecast = @forecast["forecast"]["simpleforecast"]["forecastday"][0]
+    current_weather = @forecast["current_observation"]
+
+    # Create our own hash with just the data we need
+  	@my_weather = { 
+      "location_display"    => current_weather["display_location"]["full"],
+      "current_temp"        => current_weather["temp_f"],
+      "current_conditions"  => current_weather["weather"],
+      "conditions_image"    => current_weather["icon_url"], 
+      "todays_high"         => @todays_forecast["high"]["fahrenheit"],
+      "todays_low"          => @todays_forecast["low"]["fahrenheit"]
+    }
+
+    if @my_weather["current_temp"] < 50
+      @recommedation = "Don't bike!"
+    else
+      @recommedation = "Enjoy biking!"
+    end
   end
 
 	private 
