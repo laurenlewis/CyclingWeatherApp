0d6dc55c8c7b4dc68513fbdb7b6e16e4db725acb
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-29 16:09:02 -0500

    mainly made changes to views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151124140829) do
 
   create_table "items", force: :cascade do |t|
@@ -21,9 +20,6 @@ ActiveRecord::Schema.define(version: 20151124140829) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
-
-
-ActiveRecord::Schema.define(version: 20151120185325) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -53,5 +49,4 @@ ActiveRecord::Schema.define(version: 20151120185325) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
-# 5cb4f17dfb2bebeb89b39d32536de1906b6c99bd
 end
