4750ff56d93522438fd7d992a60a5936a9f6632e
Nancy Blayney <nblayney@g.cofc.edu>
2015-12-01 23:31:12 -0500

    added a delete button and a delete method to the item model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
 
+
 def index
  @items = Item.all
 end
@@ -13,15 +14,21 @@ def create
   	@item = Item.new(item_params)
   	if @item.save
   	  flash[:success] = "New item added to your inventory"
-  	  redirect_to root_url
+  	  redirect_to items_path
   	else
   	  flash[:danger] = "Item not created"
-  	  redirect_to item_path
+  	  redirect_to items_path
   	end
 end
 
 def show
 	@items = Item.find(params[:id])
+end
+
+def destroy
+  Item.find(params[:id]).destroy
+  flash[:success] = "Item Removed from Inventory"
+  redirect_to items_path
 end
 
 
