b4143eb9813e8a80e826e098340f1b53b03e4a4a
Sara Canfield <slcanfield1@verizon.net>
2015-12-02 15:44:35 -0500

    added authenticate user to items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-before_filter :require_no_authentication, only: :new
+# before_filter :require_no_authentication, only: :new
 before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
